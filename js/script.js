9fd4eaa0497529001a163bf059f7955451b89a6b
Barbara Marcinkowska <kropisz_barbara@op.pl>
2023-11-08 12:31:04 +0100

    added new functions js

--- js/script.js
+++ js/script.js
@@ -1,45 +1,58 @@
-console.log("Cześć to ja Basia probuje coś działać");
 
-let formElement = document.querySelector(".js-form");
-let currencyElement = document.querySelector(".js-currency");
-let amountElement = document.querySelector(".js-amount");
-let resultElement = document.querySelector (".js-result");
+    const welcome = () => {
+      console.log("Cześć to ja Basia probuje coś działać.Pozdrawiam Wszystkich :)");
+    }
 
+    welcome(); 
 
-let EURO = 4.46;
-let USD = 4.07;
-let GBP = 5.17;
+{
+const formElement = document.querySelector(".js-form");
+const currencyElement = document.querySelector(".js-currency");
+const amountElement = document.querySelector(".js-amount");
+const resultElement = document.querySelector (".js-result");
+
+ const calculateResult = (amount, currency) => { 
+    const EURO = 4.46;
+    const USD = 4.07;
+    const GBP = 5.17;
+
+    switch (currency) { 
+        case "EURO":
+            return amount / EURO; 
+           
+        case "USD":
+            return amount / USD; 
+            
+        case "GBP":
+            return amount / GBP;   
+    }
+};
+calculateResult (); 
+
+//const EURO = 4.46;
+//const USD = 4.07;
+//const GBP = 5.17;
 
 formElement.addEventListener ("submit" , (event) => { 
     event.preventDefault(); 
 
-let amount = +amountElement.value;
-let currency = currencyElement.value;
-let result; 
+const amount = +amountElement.value;
+const currency = currencyElement.value;
 
-switch (currency) { 
-    case "EURO" :
-        result = amount / EURO; 
-        break; 
-    case "USD" :
-        result = amount / USD; 
-        break; 
-    case "GBP" :
-        result = amount / GBP; 
-        break; 
+const result = calculateResult (amount, currency); 
+
+//switch (currency) { 
+   // case "EURO" :
+     //   result = amount / EURO; 
+       // break; 
+   // case "USD" :
+   //     result = amount / USD; 
+   //     break; 
+   // case "GBP" :
+    //    result = amount / GBP; 
+    //    break; 
+//}
+resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong> ${result.toFixed(2)} ${currency} </strong>`;
+}) 
+
 }
-resultElement.innerHTML = ` <strong> ${result.toFixed(2)} ${currency} </strong>`; 
-})
-
-// ${amount.toFixed(2)} PLN
-
-//let button = document.querySelector(".button");
-//let body = document.querySelector(".body");
-//let nextColor = document.querySelector(".nextColor");
-
- 
-// button.addEventListener("click", () => {
-   // body.classList.toggle ("inerBody");
-
-  //  nextColor.innerText = body.classList.contains("inerBody") ? "ciemny" : "jasny";
-// });
